bb62e26a881e0c60d4e49d95b3eb55036e81071c
Joe Furfaro <contact@joefurfaro.ca>
2021-10-14 19:09:02 -0400

    Add semantics checking for font and color loading

--- src/classes/parser/Definitions.py
+++ src/classes/parser/Definitions.py
@@ -1,13 +1,59 @@
-class DefLookup:
+class ColorDef:
+    def __init__(self, ID : str, hex : str):
+        self.ID = ID # Identifier
+        self.hex = hex # Hex code in the form "#AAAAAA"
 
+class FontDef:
+    def __init__(self, ID : str, font_name : str, font_size : int, font_weight : str = "regular"):
+        self.ID = ID # Identifier
+        self.font_name = font_name # Font name
+        self.font_size = font_size # Font size
+
+class TypeDef:
+    def __init__(self, ID : str, width : int = None, height : int = None):
+        self.ID = ID # Identifier
+        self.width = width # Preset width of rect
+        self.height = height # Preset height of rect
+        self.tree = [] # Tree of finalized objects the type contains
+
+class CanvasDef:
+    def __init__(self, file : str, width : int, height : int):
+        self.file = file # File name to export to
+        self.width = width # Width of canvas
+        self.height = height # Height of canvas
+        self.tree = [] # Tree of finalized objects the canvas contains
+
+class DefLookup:
     def __init__(self):
         self.colors = []
         self.fonts = []
         self.types = []
         self.canvases = []
 
-class ColorDef:
+    def add(self, x):
+        if type(x) == ColorDef:
+            self.colors.append(x)
+        elif type(x) == FontDef:
+            self.fonts.append(x)
+        elif type(x) == TypeDef:
+            self.types.append(x)
+        elif type(x) == CanvasDef:
+            self.canvases.append(x)
+        else: 
+            raise TypeError("Attempted to add something that is not a definition to the definition lookup table")
 
-    def __init__(self, ID : str, hex : str):
-        self.ID = ID
-        self.hex = hex
+    # Check if a color with a given identifier exists
+    def has_color(self, ID):
+        return any([x.ID == ID for x in self.colors])
+
+    # Check if a font with a given identifier exists
+    def has_font(self, ID):
+        return any([x.ID == ID for x in self.fonts])
+
+    # Check if a type with a given identifier exists
+    def has_type(self, ID):
+        return any([x.ID == ID for x in self.types])
+
+    # Check if a canvas with a given file name exists
+    def has_type(self, file):
+        return any([x.file == file for x in self.canvases])
--- src/main.py
+++ src/main.py
@@ -1,10 +1,65 @@
 #!/usr/bin/python
 import sys
+from classes.parser.Definitions import *
+
 try:
     from lark import Lark, Tree
 except:
     print("Please run `pip install lark --upgrade` and start the program again")
     exit()
+
+class Parser:
+    def __init__(self, grammar : str):
+        self.grammar = grammar
+        self.defs = DefLookup() # Definition lookup table
+
+    def exception(self, line, col, msg):
+        print("Exception on line " + str(line) + " column " + str(col) + ": " + str(msg))
+        sys.exit(1)
+
+    def parse(self, code : str):
+        try:
+            l = Lark(self.grammar)
+            parseTree = l.parse(code)
+        except Exception as e:
+            print(e) # TODO return the error in a nice way
+            exit()
+
+        # print(parseTree)
+
+        color_defs = [x for x in parseTree.children if x.data == "color_definition"]
+        font_defs = [x for x in parseTree.children if x.data == "font_definition"]
+        type_defs = [x for x in parseTree.children if x.data == "type_definition"]
+        canvas_defs = [x for x in parseTree.children if x.data == "canvas_definition"]
+
+        self.process_color_defs(color_defs)
+        self.process_font_defs(font_defs)
+
+        # TODO semantic checking
+
+        # TODO build tree
+
+    def process_color_defs(self, color_defs):
+        for x in color_defs:
+            ID = x.children[0].children[0]
+            hex_code = x.children[1].children[0]
+            if self.defs.has_color(ID.value):
+                self.exception(ID.line, ID.column, ": Color identifier '" + ID.value + "' is already in use")
+            self.defs.add(ColorDef(ID.value, hex_code.value))
+
+    def process_font_defs(self, font_defs):
+        for x in font_defs:
+            ID = x.children[0].children[0]
+            font_name = x.children[1].children[0][1:-1]
+            font_size = int(x.children[2].children[0])
+            font_weight = "regular"
+            if len(x.children) > 3:
+                font_weight = x.children[3].children[0].value
+            if self.defs.has_font(ID):
+                self.exception(ID.line, ID.column, ": Font identifier '" + ID.value + "' is already in use")
+            self.defs.add(FontDef(ID.value, font_name, font_size, font_weight))
+        print(self.defs.fonts)
+
 
 def main():
     fileToParse = ""
@@ -17,31 +72,8 @@ def main():
         exit()
 
     with open(fileToParse) as fileContents, open("global_grammar.lark") as global_grammar:
-
         code = str(fileContents.read()) + "\n" # add a new line to ease grammar checking (NOTE: breaks math grammar when it is by itself)
-
-        try:
-            parseTree = parseCode(code, global_grammar)
-        except Exception as e:
-            print(e) # TODO return the error in a nice way
-            exit()
-
-        print(parseTree)
-
-        color_defs = [x for x in parseTree.children if x.data == "color_definition"]
-        font_defs = [x for x in parseTree.children if x.data == "font_definition"]
-        type_defs = [x for x in parseTree.children if x.data == "type_definition"]
-        canvas_defs = [x for x in parseTree.children if x.data == "canvas_definition"]
-
-        # TODO semantic checking
-
-        # TODO build tree
-
-def parseCode(code: str, grammar: str):
-    l = Lark(grammar)
-
-    output = l.parse(code)
-
-    return output
+        p = Parser(global_grammar)
+        p.parse(code)
     
 main()
