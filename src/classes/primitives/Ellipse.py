8923dc42cf91b860f609386bf4bae4467d3a3c7d
AstroCoding <37601898+AstroCoding@users.noreply.github.com>
2021-10-15 13:44:15 -0400

    Begin work on Ellipse

--- src/classes/primitives/Ellipse.py
+++ src/classes/primitives/Ellipse.py
@@ -1,7 +1,45 @@
-import Shape
+from typing import Union, Optional
+from collections.abc import Iterable
+from ..Stroke import Stroke
+from ..Fill import Fill
+from ..Item import Item
+from .Shape import Shape
+from ..util.Vector2 import Vector2
+
 
 class Ellipse(Shape):
-    def __init__(self, position : tuple, x_radius : float, y_radius : float):
-        self.position = position
-        self.xR = x_radius
-        self.yR = y_radius
+    """
+    Ellipse shape primitive:
+    SVG ellipse is center alligned be default.
+    """
+    # create circle as an ellipse with same x and y radius
+
+    def __init__(self,
+                 clipped: bool, position: Vector2,
+                 rx: Union[int, float], ry: Union[int, float],
+                 stroke: Optional[Stroke] = None, fill: Optional[Fill] = None,
+                 children: Iterable[Item] = [], rotation: Union[int, float] = 0
+                 ):
+        super().__init__(clipped, position,
+                         stroke=stroke, fill=fill,
+                         children=children, rotation=rotation)
+        self.rx: Union[int, float] = rx
+        self.ry: Union[int, float] = ry
+
+    def render(self) -> str:
+        # Create circle SVG instance
+        s = f'<ellipse cx="{self.position.x}" cy="{self.position.y}" rx="{self.rx}" ry="{self.ry}"'
+        # Apply rotation if needed
+        if abs(self.rotation) > 1e-6:
+            s += f' transform="rotate({self.rotation} {self.position.x} {self.position.y})"'
+        # Apply stroke and fill
+        if self.stroke != None:
+            s += ' ' + self.stroke.render()
+        if self.fill != None:
+            s += ' ' + self.fill.render()
+        s += ' />'
+        # Render Children
+        if len(self.children) > 0:
+            s += '\n'
+            s += self.render_children(self.position, self.depth, self.children)
+        return s
