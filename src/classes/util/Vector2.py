83ddbef9daa068111ff86fa1809ce9f85f1ba0cb
larry156 <yaol13@mcmaster.ca>
2021-10-15 13:12:49 -0400

    Added some additional functionality for Vector2

--- src/classes/util/Vector2.py
+++ src/classes/util/Vector2.py
@@ -1,4 +1,7 @@
 class Vector2:
+    """
+    Class representing two-dimensional vectors.
+    """
     def __init__(self, x: int = 0, y: int = 0):
         self.x: int = x
         self.y: int = y
@@ -17,3 +20,16 @@ class Vector2:
             return Vector2(self.x + other.x, self.y + other.y)
         else:
             raise TypeError
+
+    __radd__ = __add__
+
+    def __sub__(self, other: object) -> object:
+        if isinstance(other, self.__class__):
+            return Vector2(self.x - other.x, self.y - other.y)
+        else:
+            raise TypeError
+
+    def __mul__(self, other: float) -> object:
+        return Vector2(self.x * other, self.y * other)
+
+    __rmul__ = __mul__
