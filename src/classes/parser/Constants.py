345d9db2d403b7c83ea2e0d7be3ac5477d430c64
Daniel-Genkin <genkind@gmail.com>
2021-10-31 16:14:49 -0400

    initial proof of concept commit

---
+++ src/classes/parser/Constants.py
@@ -0,0 +1,76 @@
+
+# AUTO-GENERATED FILE. DO NOT MODIFY DIRECTLY
+# To regenerate run `python3 ConstantsBuilder.py` form this directory
+
+from enum import Enum
+
+def asList(enum):
+    result = []
+    for val in enum:
+        result.append(val.value)
+    return result
+
+class IMG_FORMATS(Enum):
+    JPG = "jpg"
+    JPEG = "jpeg"
+    PNG = "png"
+    SVG = "svg"
+
+class DEFS (Enum):
+	 COLOR = "color_definition"
+	 FONT = "font_definition"
+	 TYPE = "type_definition"
+	 CANVAS = "canvas_definition"
+
+class ATTRIBS (Enum):
+	 START = "start"
+	 IDENTIFIER = "identifier"
+	 HEX_COLOR = "hex_color"
+	 FONT_NAME = "font_name"
+	 FONT_SIZE = "font_size"
+	 FONT_WEIGHT = "font_weight"
+	 INT_WIDTH = "int_width"
+	 INT_HEIGHT = "int_height"
+	 ITEM_LIST = "item_list"
+	 FILE_NAME = "file_name"
+	 ITEM = "item"
+	 COLOR = "color"
+	 CLIP = "clip"
+	 MODIFIER_LIST = "modifier_list"
+	 MODIFIER = "modifier"
+	 OUTLINED = "outlined"
+	 ROTATED = "rotated"
+	 CLIPPED = "clipped"
+	 UNIT = "unit"
+	 EXPRESSION = "expression"
+	 POSITION = "position"
+	 SIZE = "size"
+	 THICKNESS = "thickness"
+	 OPACITY = "opacity"
+	 ANGLE = "angle"
+	 FONT = "font"
+	 CONTENT = "content"
+	 PATH = "path"
+	 RECT = "rect"
+	 CIRCLE = "circle"
+	 ELLIPSE = "ellipse"
+	 LINE = "line"
+	 POLYGON = "polygon"
+	 TEXT = "text"
+	 IMAGE = "image"
+	 CUSTOM = "custom"
+
+class FONT_WEIGHT (Enum):
+	 THIN = "thin"
+	 REGULAR = "regular"
+	 BOLD = "bold"
+
+class CLIP_TYPE (Enum):
+	 IN = "in"
+	 OUT = "out"
+
+class UNIT (Enum):
+	 H = "h"
+	 W = "w"
+
+
---
+++ src/classes/parser/ConstantsBuilder.py
@@ -0,0 +1,82 @@
+# all definitions must end with this suffix
+definitionSuffix = "_definition"
+
+definitions = []
+attributes = []
+enums = []
+with open("../../global_grammar.lark") as grammar: # TODO use regex?
+    while (line := grammar.readline()):
+        line = line.strip()
+        if (line.startswith("_") or len(line) == 0):
+            continue
+
+        identifierEndIndex = line.find(":")
+        identifier = line[0:identifierEndIndex]
+
+        # if the line is a definition
+        if (identifier.endswith(definitionSuffix)):
+            definitions.append((identifier[0:identifier.find(definitionSuffix)].upper(), identifier))
+            currentEnum = ""
+
+        # if the line is an identifier (TODO find a better option tan valid ptyhon identifier)
+        elif (identifier.isidentifier() and identifier.islower()):
+            attributes.append((identifier.upper(), identifier))
+
+        # if the line is a terminal (i.e. enum) # TODO currently all enums values must be on 1 line
+        elif (identifier.isidentifier() and identifier.isupper() and line.__contains__("|")):
+            enums.append((identifier, line[identifierEndIndex+1:].replace("\"","").replace(" ", "").split("|")))
+
+# TODO now loop over and make sure we don't have any enums that are marked as "%ignore"
+
+
+# TODO generate the Constants.py file
+header = '''# AUTO-GENERATED FILE. DO NOT MODIFY DIRECTLY
+# To regenerate run `python3 ConstantsBuilder.py` form this directory
+
+from enum import Enum
+
+def asList(enum):
+    result = []
+    for val in enum:
+        result.append(val.value)
+    return result
+'''
+
+extraConstants = '''class IMG_FORMATS(Enum):
+    JPG = "jpg"
+    JPEG = "jpeg"
+    PNG = "png"
+    SVG = "svg"
+'''
+
+strDefs = "class DEFS (Enum):\n"
+
+for d in definitions:
+    strDefs += f"\t {d[0]} = \"{d[1]}\"\n"
+
+
+strAttribs = "class ATTRIBS (Enum):\n"
+
+for a in attributes:
+    strAttribs += f"\t {a[0]} = \"{a[1]}\"\n"
+
+strEnums = ""
+
+for e in enums:
+    strEnums += f"class {e[0]} (Enum):\n"
+    for v in e[1]:
+        strEnums += f"\t {v.upper()} = \"{v}\"\n"
+    strEnums += "\n"
+
+
+output = f'''
+{header}
+{extraConstants}
+{strDefs}
+{strAttribs}
+{strEnums}
+'''
+
+f = open("Constants.py", "w")
+f.write(output)
+f.close()
